c806bf02f0a43246321c250727908a7a6c4a9805
altuga@gmail.com <upux1234>
2017-12-14 01:01:12 +0300

    bolum 1 degisikleri

---
+++ bolum1/src/main/java/com.kodcu/SelamDoc.java
@@ -0,0 +1,22 @@
+/** Ilk Java Kodumuzun Dokumantasyonu
+ * Ekrana Selamlar diyen bir uygulama  
+ * @see #main
+ * @author Altug B. Altintas (altug@kodcu.com)
+ * @version 1.0 
+ * @since 2017
+*/
+public class SelamDoc {
+  
+  
+  /**sayiyi artirmak icin , degiskenler icin bir ornek*/ 
+   public int sayac = 0 ;
+  
+  
+  /** siniflarda & uygulumalarda giris noktasi olan metod
+   * @param args disaradan girilen parameterler dizisi
+   * @return donen deger yok   
+   * @exception Hic istisna firlatilmiyor  */ 
+   public static void main(String[] args) {    
+	     System.out.println("Selamlar !");
+   }
+ }
