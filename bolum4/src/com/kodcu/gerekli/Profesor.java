f68a74ab37dac500440a89f794d43c11bbd66eb5
altuga@gmail.com <upux1234>
2018-01-11 11:11:44 +0300

    bolum 4 degisikleri

---
+++ bolum4/src/com/kodcu/gerekli/Profesor.java
@@ -0,0 +1,12 @@
+package com.kodcu.gerekli;
+class Profesor {   
+	public static void main(String[] args){
+
+	new Profesor().kullan();
+	}
+	
+    public void kullan() {
+	    Robot  upuaut = new Robot(); // sorunsuz
+    }
+}
+
