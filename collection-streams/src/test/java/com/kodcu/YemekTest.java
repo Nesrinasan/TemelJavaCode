bed3071e17604a91354b04935af0c0c5004f6a50
altuga@gmail.com <upux1234>
2018-02-03 17:50:44 +0300

    partitioningBy kısmı eklendi

--- collection-streams/src/test/java/com/kodcu/YemekTest.java
+++ collection-streams/src/test/java/com/kodcu/YemekTest.java
@@ -11,5 +11,19 @@ public class YemekTest {
         String beklenenSonuc = "{BALIK=[kalamar, somon], DIGER=[kizarmis patates, pilav, meyve, pizza], ET=[rosto, biftek, tavuk]}";
 
 
+
+    }
+
+    public void vejeteryanOlupOlmadiklarinaGoreBolumle() {
+
+        // TODO
+
+        String beklenenSonuc = "{false=[rosto, biftek, tavuk, kalamar, somon], true=[kizarmis patates, pilav, meyve, pizza]}";
+    }
+
+    public void vejeteryanOlupOlmadiklarinaGoreBolumleVeEnYuksekKaloriliYiyecekleriGetir() {
+
+        // TODO
+
     }
 }
