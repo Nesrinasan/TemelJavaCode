7371e06d8230352d46461a541cedb410e953d73a
altuga@gmail.com <upux1234>
2018-03-31 15:03:02 +0300

    reduce örnekler

--- collection-streams/src/test/java/com/kodcu/YemekTest.java
+++ collection-streams/src/test/java/com/kodcu/YemekTest.java
@@ -1,6 +1,11 @@
 package com.kodcu;
 
 
+import com.kodcu.streams.Yemek;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Optional;
 
 public class YemekTest {
 
@@ -21,9 +26,17 @@ public class YemekTest {
         String beklenenSonuc = "{false=[rosto, biftek, tavuk, kalamar, somon], true=[kizarmis patates, pilav, meyve, pizza]}";
     }
 
-    public void vejeteryanOlupOlmadiklarinaGoreBolumleVeEnYuksekKaloriliYiyecekleriGetir() {
+    @Test
+    public void kolariHesapla() {
 
         // TODO
+
+        int kalori = Yemek.menu.stream().mapToInt(yemek -> yemek.getKalorisi()).sum();
+        Assert.assertEquals(4300, kalori );
+
+        Optional<Integer> kolariIki = Yemek.menu.stream().map(yemek -> yemek.getKalorisi()).reduce(Integer::sum);
+        kolariIki.ifPresent(System.out::print);
+        Assert.assertEquals(4300, kolariIki.get().intValue() );
 
     }
 }
